b01284d6aa776ca2a2a2a6fb7b11bd07ad949ae8
olegarxo <olegarxo15@gmail.com>
2022-07-25 15:46:59 +0300

    Сделал второе задание

---
+++ homework/homework_9/task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Число М = ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Число N = ");
+int numberN = int.Parse(Console.ReadLine());
+int sum = 0;
+int count = numberN - numberM;
+OutSummumber (numberM, numberN, sum);
+void OutSummumber(int numberOne, int numberTwo, int sum = 0)
+{
+    sum += numberOne;
+    numberOne++;
+    if (numberOne > numberTwo) 
+    {
+    Console.WriteLine(sum);
+    return;
+    }
+    OutSummumber(numberOne, numberN, sum);
+}
