b8276a125c29e0f92ec6ec69b17cfea57f552cbd
oleg <olegarxo15@gmail.com>
2022-07-27 15:22:20 +0300

    Сделал третье доп задание

---
+++ dophomework/dophomework_6/task_tree/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите проверяемое число: ");
+double enterNum = double.Parse(Console.ReadLine());
+double num = enterNum;
+while(true)
+{
+    if(enterNum / 3 == 1)
+    {
+        Console.WriteLine($"{num} является степерью числа 3");
+        break;
+    }
+    else if (enterNum < 1)
+    {
+        Console.WriteLine($"{num} не является степерью числа");
+        break;
+    }
+    enterNum = enterNum/3;
+}
